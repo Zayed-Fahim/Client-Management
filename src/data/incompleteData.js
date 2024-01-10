f3f869c21e1d239084192e81a63505f89112608e
Zayed Fahim <zayedfahim33.cse@gmail.com>
2024-01-11 03:13:08 +0600

    design full complete

---
+++ src/data/incompleteData.js
@@ -0,0 +1,324 @@
+const incompleteData = [
+  {
+    myInfo: {
+      name: "Zayed Fahim",
+      image: "https://i.ibb.co/gmnzmTL/1.png",
+    },
+    clientInfo: {
+      name: "Client Name",
+      image:
+        "https://plus.unsplash.com/premium_photo-1682089892133-556bde898f2c?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8aW5kaWFuJTIwYm95fGVufDB8fDB8fHww",
+    },
+    details: {
+      text: "Lorem ipsum dolor sit amet consectetur adipiscing elit litora sodales turpis, nam lobortis ultrices sagittis ad nisl lacus vulputate. Mi non sollicitudin massa fusce, iaculis commodo primis eu vitae, platea vestibulum montes. Primis ut sem mauris eu per parturient quis dictumst, porttitor sociosqu augue eros gravida dui facilisi, sed mattis eget erat tristique non congue. Accumsan netus sed natoque tempor enim erat pulvinar, torquent condimentum imperdiet tempus quisque magna donec velit, nullam semper interdum scelerisque montes porta.",
+      total_task: 2,
+      task_remaining: 1,
+    },
+    contributors: [
+      {
+        name: "Unknown1",
+        image:
+          "https://img.freepik.com/premium-photo/portrait-happy-indian-teenager-college-school-boy-with-backpack-holding-books-isolated-whi_908985-9250.jpg",
+      },
+      {
+        name: "Unknown2",
+        image:
+          "https://media.istockphoto.com/id/1372481295/photo/photo-of-school-boy-wear-yellow-t-shirt-backpack-in-background-stock-photo.jpg?s=612x612&w=0&k=20&c=lRsVHozgtzpj9W6ZjEW2g2qTkh4iV4BLePFgvUt7kvE=",
+      },
+    ],
+    notifications: 15,
+    messages: 15,
+    attachments: 25,
+    date: "10-12-2023",
+  },
+  {
+    myInfo: {
+      name: "Zayed Fahim",
+      image: "https://i.ibb.co/gmnzmTL/1.png",
+    },
+    clientInfo: {
+      name: "Client Name",
+      image:
+        "https://plus.unsplash.com/premium_photo-1682089892133-556bde898f2c?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8aW5kaWFuJTIwYm95fGVufDB8fDB8fHww",
+    },
+    details: {
+      text: "Lorem ipsum dolor sit amet consectetur adipiscing elit litora sodales turpis, nam lobortis ultrices sagittis ad nisl lacus vulputate. Mi non sollicitudin massa fusce, iaculis commodo primis eu vitae, platea vestibulum montes. Primis ut sem mauris eu per parturient quis dictumst, porttitor sociosqu augue eros gravida dui facilisi, sed mattis eget erat tristique non congue. Accumsan netus sed natoque tempor enim erat pulvinar, torquent condimentum imperdiet tempus quisque magna donec velit, nullam semper interdum scelerisque montes porta.",
+      total_task: 2,
+      task_remaining: 1,
+    },
+    contributors: [
+      {
+        name: "Unknown1",
+        image:
+          "https://img.freepik.com/premium-photo/portrait-happy-indian-teenager-college-school-boy-with-backpack-holding-books-isolated-whi_908985-9250.jpg",
+      },
+      {
+        name: "Unknown2",
+        image:
+          "https://media.istockphoto.com/id/1372481295/photo/photo-of-school-boy-wear-yellow-t-shirt-backpack-in-background-stock-photo.jpg?s=612x612&w=0&k=20&c=lRsVHozgtzpj9W6ZjEW2g2qTkh4iV4BLePFgvUt7kvE=",
+      },
+    ],
+    notifications: 10,
+    messages: 15,
+    attachments: 25,
+    date: new Date(),
+  },
+  {
+    myInfo: {
+      name: "Zayed Fahim",
+      image: "https://i.ibb.co/gmnzmTL/1.png",
+    },
+    clientInfo: {
+      name: "Client Name",
+      image:
+        "https://plus.unsplash.com/premium_photo-1682089892133-556bde898f2c?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8aW5kaWFuJTIwYm95fGVufDB8fDB8fHww",
+    },
+    details: {
+      text: "Lorem ipsum dolor sit amet consectetur adipiscing elit litora sodales turpis, nam lobortis ultrices sagittis ad nisl lacus vulputate. Mi non sollicitudin massa fusce, iaculis commodo primis eu vitae, platea vestibulum montes. Primis ut sem mauris eu per parturient quis dictumst, porttitor sociosqu augue eros gravida dui facilisi, sed mattis eget erat tristique non congue. Accumsan netus sed natoque tempor enim erat pulvinar, torquent condimentum imperdiet tempus quisque magna donec velit, nullam semper interdum scelerisque montes porta.",
+      total_task: 2,
+      task_remaining: 1,
+    },
+    contributors: [
+      {
+        name: "Unknown1",
+        image:
+          "https://img.freepik.com/premium-photo/portrait-happy-indian-teenager-college-school-boy-with-backpack-holding-books-isolated-whi_908985-9250.jpg",
+      },
+      {
+        name: "Unknown2",
+        image:
+          "https://media.istockphoto.com/id/1372481295/photo/photo-of-school-boy-wear-yellow-t-shirt-backpack-in-background-stock-photo.jpg?s=612x612&w=0&k=20&c=lRsVHozgtzpj9W6ZjEW2g2qTkh4iV4BLePFgvUt7kvE=",
+      },
+    ],
+    notifications: 10,
+    messages: 15,
+    attachments: 25,
+    date: new Date(),
+  },
+  {
+    myInfo: {
+      name: "Zayed Fahim",
+      image: "https://i.ibb.co/gmnzmTL/1.png",
+    },
+    clientInfo: {
+      name: "Client Name",
+      image:
+        "https://plus.unsplash.com/premium_photo-1682089892133-556bde898f2c?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8aW5kaWFuJTIwYm95fGVufDB8fDB8fHww",
+    },
+    details: {
+      text: "Lorem ipsum dolor sit amet consectetur adipiscing elit litora sodales turpis, nam lobortis ultrices sagittis ad nisl lacus vulputate. Mi non sollicitudin massa fusce, iaculis commodo primis eu vitae, platea vestibulum montes. Primis ut sem mauris eu per parturient quis dictumst, porttitor sociosqu augue eros gravida dui facilisi, sed mattis eget erat tristique non congue. Accumsan netus sed natoque tempor enim erat pulvinar, torquent condimentum imperdiet tempus quisque magna donec velit, nullam semper interdum scelerisque montes porta.",
+      total_task: 2,
+      task_remaining: 1,
+    },
+    contributors: [
+      {
+        name: "Unknown1",
+        image:
+          "https://img.freepik.com/premium-photo/portrait-happy-indian-teenager-college-school-boy-with-backpack-holding-books-isolated-whi_908985-9250.jpg",
+      },
+      {
+        name: "Unknown2",
+        image:
+          "https://media.istockphoto.com/id/1372481295/photo/photo-of-school-boy-wear-yellow-t-shirt-backpack-in-background-stock-photo.jpg?s=612x612&w=0&k=20&c=lRsVHozgtzpj9W6ZjEW2g2qTkh4iV4BLePFgvUt7kvE=",
+      },
+    ],
+    notifications: 10,
+    messages: 15,
+    attachments: 25,
+    date: new Date(),
+  },
+  {
+    myInfo: {
+      name: "Zayed Fahim",
+      image: "https://i.ibb.co/gmnzmTL/1.png",
+    },
+    clientInfo: {
+      name: "Client Name",
+      image:
+        "https://plus.unsplash.com/premium_photo-1682089892133-556bde898f2c?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8aW5kaWFuJTIwYm95fGVufDB8fDB8fHww",
+    },
+    details: {
+      text: "Lorem ipsum dolor sit amet consectetur adipiscing elit litora sodales turpis, nam lobortis ultrices sagittis ad nisl lacus vulputate. Mi non sollicitudin massa fusce, iaculis commodo primis eu vitae, platea vestibulum montes. Primis ut sem mauris eu per parturient quis dictumst, porttitor sociosqu augue eros gravida dui facilisi, sed mattis eget erat tristique non congue. Accumsan netus sed natoque tempor enim erat pulvinar, torquent condimentum imperdiet tempus quisque magna donec velit, nullam semper interdum scelerisque montes porta.",
+      total_task: 2,
+      task_remaining: 1,
+    },
+    contributors: [
+      {
+        name: "Unknown1",
+        image:
+          "https://img.freepik.com/premium-photo/portrait-happy-indian-teenager-college-school-boy-with-backpack-holding-books-isolated-whi_908985-9250.jpg",
+      },
+      {
+        name: "Unknown2",
+        image:
+          "https://media.istockphoto.com/id/1372481295/photo/photo-of-school-boy-wear-yellow-t-shirt-backpack-in-background-stock-photo.jpg?s=612x612&w=0&k=20&c=lRsVHozgtzpj9W6ZjEW2g2qTkh4iV4BLePFgvUt7kvE=",
+      },
+    ],
+    notifications: 10,
+    messages: 15,
+    attachments: 25,
+    date: new Date(),
+  },
+  {
+    myInfo: {
+      name: "Zayed Fahim",
+      image: "https://i.ibb.co/gmnzmTL/1.png",
+    },
+    clientInfo: {
+      name: "Client Name",
+      image:
+        "https://plus.unsplash.com/premium_photo-1682089892133-556bde898f2c?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8aW5kaWFuJTIwYm95fGVufDB8fDB8fHww",
+    },
+    details: {
+      text: "Lorem ipsum dolor sit amet consectetur adipiscing elit litora sodales turpis, nam lobortis ultrices sagittis ad nisl lacus vulputate. Mi non sollicitudin massa fusce, iaculis commodo primis eu vitae, platea vestibulum montes. Primis ut sem mauris eu per parturient quis dictumst, porttitor sociosqu augue eros gravida dui facilisi, sed mattis eget erat tristique non congue. Accumsan netus sed natoque tempor enim erat pulvinar, torquent condimentum imperdiet tempus quisque magna donec velit, nullam semper interdum scelerisque montes porta.",
+      total_task: 2,
+      task_remaining: 1,
+    },
+    contributors: [
+      {
+        name: "Unknown1",
+        image:
+          "https://img.freepik.com/premium-photo/portrait-happy-indian-teenager-college-school-boy-with-backpack-holding-books-isolated-whi_908985-9250.jpg",
+      },
+      {
+        name: "Unknown2",
+        image:
+          "https://media.istockphoto.com/id/1372481295/photo/photo-of-school-boy-wear-yellow-t-shirt-backpack-in-background-stock-photo.jpg?s=612x612&w=0&k=20&c=lRsVHozgtzpj9W6ZjEW2g2qTkh4iV4BLePFgvUt7kvE=",
+      },
+    ],
+    notifications: 10,
+    messages: 15,
+    attachments: 25,
+    date: new Date(),
+  },
+  {
+    myInfo: {
+      name: "Zayed Fahim",
+      image: "https://i.ibb.co/gmnzmTL/1.png",
+    },
+    clientInfo: {
+      name: "Client Name",
+      image:
+        "https://plus.unsplash.com/premium_photo-1682089892133-556bde898f2c?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8aW5kaWFuJTIwYm95fGVufDB8fDB8fHww",
+    },
+    details: {
+      text: "Lorem ipsum dolor sit amet consectetur adipiscing elit litora sodales turpis, nam lobortis ultrices sagittis ad nisl lacus vulputate. Mi non sollicitudin massa fusce, iaculis commodo primis eu vitae, platea vestibulum montes. Primis ut sem mauris eu per parturient quis dictumst, porttitor sociosqu augue eros gravida dui facilisi, sed mattis eget erat tristique non congue. Accumsan netus sed natoque tempor enim erat pulvinar, torquent condimentum imperdiet tempus quisque magna donec velit, nullam semper interdum scelerisque montes porta.",
+      total_task: 2,
+      task_remaining: 1,
+    },
+    contributors: [
+      {
+        name: "Unknown1",
+        image:
+          "https://img.freepik.com/premium-photo/portrait-happy-indian-teenager-college-school-boy-with-backpack-holding-books-isolated-whi_908985-9250.jpg",
+      },
+      {
+        name: "Unknown2",
+        image:
+          "https://media.istockphoto.com/id/1372481295/photo/photo-of-school-boy-wear-yellow-t-shirt-backpack-in-background-stock-photo.jpg?s=612x612&w=0&k=20&c=lRsVHozgtzpj9W6ZjEW2g2qTkh4iV4BLePFgvUt7kvE=",
+      },
+    ],
+    notifications: 10,
+    messages: 15,
+    attachments: 25,
+    date: new Date(),
+  },
+  {
+    myInfo: {
+      name: "Zayed Fahim",
+      image: "https://i.ibb.co/gmnzmTL/1.png",
+    },
+    clientInfo: {
+      name: "Client Name",
+      image:
+        "https://plus.unsplash.com/premium_photo-1682089892133-556bde898f2c?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8aW5kaWFuJTIwYm95fGVufDB8fDB8fHww",
+    },
+    details: {
+      text: "Lorem ipsum dolor sit amet consectetur adipiscing elit litora sodales turpis, nam lobortis ultrices sagittis ad nisl lacus vulputate. Mi non sollicitudin massa fusce, iaculis commodo primis eu vitae, platea vestibulum montes. Primis ut sem mauris eu per parturient quis dictumst, porttitor sociosqu augue eros gravida dui facilisi, sed mattis eget erat tristique non congue. Accumsan netus sed natoque tempor enim erat pulvinar, torquent condimentum imperdiet tempus quisque magna donec velit, nullam semper interdum scelerisque montes porta.",
+      total_task: 2,
+      task_remaining: 1,
+    },
+    contributors: [
+      {
+        name: "Unknown1",
+        image:
+          "https://img.freepik.com/premium-photo/portrait-happy-indian-teenager-college-school-boy-with-backpack-holding-books-isolated-whi_908985-9250.jpg",
+      },
+      {
+        name: "Unknown2",
+        image:
+          "https://media.istockphoto.com/id/1372481295/photo/photo-of-school-boy-wear-yellow-t-shirt-backpack-in-background-stock-photo.jpg?s=612x612&w=0&k=20&c=lRsVHozgtzpj9W6ZjEW2g2qTkh4iV4BLePFgvUt7kvE=",
+      },
+    ],
+    notifications: 10,
+    messages: 15,
+    attachments: 25,
+    date: new Date(),
+  },
+  {
+    myInfo: {
+      name: "Zayed Fahim",
+      image: "https://i.ibb.co/gmnzmTL/1.png",
+    },
+    clientInfo: {
+      name: "Client Name",
+      image:
+        "https://plus.unsplash.com/premium_photo-1682089892133-556bde898f2c?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8aW5kaWFuJTIwYm95fGVufDB8fDB8fHww",
+    },
+    details: {
+      text: "Lorem ipsum dolor sit amet consectetur adipiscing elit litora sodales turpis, nam lobortis ultrices sagittis ad nisl lacus vulputate. Mi non sollicitudin massa fusce, iaculis commodo primis eu vitae, platea vestibulum montes. Primis ut sem mauris eu per parturient quis dictumst, porttitor sociosqu augue eros gravida dui facilisi, sed mattis eget erat tristique non congue. Accumsan netus sed natoque tempor enim erat pulvinar, torquent condimentum imperdiet tempus quisque magna donec velit, nullam semper interdum scelerisque montes porta.",
+      total_task: 2,
+      task_remaining: 1,
+    },
+    contributors: [
+      {
+        name: "Unknown1",
+        image:
+          "https://img.freepik.com/premium-photo/portrait-happy-indian-teenager-college-school-boy-with-backpack-holding-books-isolated-whi_908985-9250.jpg",
+      },
+      {
+        name: "Unknown2",
+        image:
+          "https://media.istockphoto.com/id/1372481295/photo/photo-of-school-boy-wear-yellow-t-shirt-backpack-in-background-stock-photo.jpg?s=612x612&w=0&k=20&c=lRsVHozgtzpj9W6ZjEW2g2qTkh4iV4BLePFgvUt7kvE=",
+      },
+    ],
+    notifications: 10,
+    messages: 15,
+    attachments: 25,
+    date: new Date(),
+  },
+  {
+    myInfo: {
+      name: "Zayed Fahim",
+      image: "https://i.ibb.co/gmnzmTL/1.png",
+    },
+    clientInfo: {
+      name: "Client Name",
+      image:
+        "https://plus.unsplash.com/premium_photo-1682089892133-556bde898f2c?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8aW5kaWFuJTIwYm95fGVufDB8fDB8fHww",
+    },
+    details: {
+      text: "Lorem ipsum dolor sit amet consectetur adipiscing elit litora sodales turpis, nam lobortis ultrices sagittis ad nisl lacus vulputate. Mi non sollicitudin massa fusce, iaculis commodo primis eu vitae, platea vestibulum montes. Primis ut sem mauris eu per parturient quis dictumst, porttitor sociosqu augue eros gravida dui facilisi, sed mattis eget erat tristique non congue. Accumsan netus sed natoque tempor enim erat pulvinar, torquent condimentum imperdiet tempus quisque magna donec velit, nullam semper interdum scelerisque montes porta.",
+      total_task: 2,
+      task_remaining: 1,
+    },
+    contributors: [
+      {
+        name: "Unknown1",
+        image:
+          "https://img.freepik.com/premium-photo/portrait-happy-indian-teenager-college-school-boy-with-backpack-holding-books-isolated-whi_908985-9250.jpg",
+      },
+      {
+        name: "Unknown2",
+        image:
+          "https://media.istockphoto.com/id/1372481295/photo/photo-of-school-boy-wear-yellow-t-shirt-backpack-in-background-stock-photo.jpg?s=612x612&w=0&k=20&c=lRsVHozgtzpj9W6ZjEW2g2qTkh4iV4BLePFgvUt7kvE=",
+      },
+    ],
+    notifications: 10,
+    messages: 15,
+    attachments: 25,
+    date: new Date(),
+  },
+];
+
+export default incompleteData;
